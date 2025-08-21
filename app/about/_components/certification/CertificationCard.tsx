e082bdded73b221e7bf7c13b41f6391a3a8380fe
shunsei <k.shunsei1227@gmail.com>
2025-08-21 09:11:17 +0900

    format cert card

--- app/about/_components/certification/CertificationCard.tsx
+++ app/about/_components/certification/CertificationCard.tsx
@@ -8,7 +8,11 @@ type Props = {
 
 export const CertificationCard = ({ certification }: Props) => {
 	return (
-		<div className="relative w-[380px] h-[220px] bg-gray-100 rounded-xl shadow-2xl border-2 border-gray-300 overflow-hidden transition-all hover:transform hover:-translate-y-2">
+		<div
+			className="
+			relative w-[380px] h-[220px] bg-gray-100 rounded-xl shadow-2xl border-2 border-gray-300
+			overflow-hidden transition-all hover:transform hover:-translate-y-2"
+		>
 			<CertificationCardHeader
 				title={certification.title}
 				color={certification.color}
