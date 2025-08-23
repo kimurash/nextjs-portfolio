0fea9dfd78558239cc9a36b28a6d71363e21a14a
shunsei <k.shunsei1227@gmail.com>
2025-08-23 15:21:33 +0900

    add skill constant

---
+++ app/skills/_components/constants.ts
@@ -0,0 +1,312 @@
+import type { TechnicalSkill } from "./types";
+
+export const MAX_PROFICIENCY = 5;
+
+export const operatingSystems: TechnicalSkill[] = [
+	{
+		name: "macOS",
+		icon: "https://skillicons.dev/icons?i=apple&theme=light",
+		proficiency: 4,
+		tags: ["研究", "趣味"],
+	},
+	{
+		name: "Ubuntu",
+		icon: "https://skillicons.dev/icons?i=ubuntu&theme=light",
+		proficiency: 4,
+		tags: ["研究", "趣味"],
+	},
+	{
+		name: "Windows",
+		icon: "https://skillicons.dev/icons?i=windows&theme=light",
+		proficiency: 3,
+		tags: ["授業", "研究", "趣味"],
+	},
+];
+
+export const languages: TechnicalSkill[] = [
+	{
+		name: "C言語",
+		icon: "https://skillicons.dev/icons?i=c&theme=light",
+		proficiency: 5,
+		tags: ["授業"],
+	},
+	{
+		name: "C++",
+		icon: "https://skillicons.dev/icons?i=cpp&theme=light",
+		proficiency: 3,
+		tags: ["競プロ"],
+	},
+	{
+		name: "C#",
+		icon: "https://skillicons.dev/icons?i=cs&theme=light",
+		proficiency: 3,
+		tags: ["研究"],
+	},
+	{
+		name: "CSS",
+		icon: "https://skillicons.dev/icons?i=css&theme=light",
+		proficiency: 3,
+		tags: ["趣味"],
+	},
+	{
+		name: "HTML",
+		icon: "https://skillicons.dev/icons?i=html&theme=light",
+		proficiency: 4,
+		tags: ["授業", "趣味"],
+	},
+	{
+		name: "JavaScript",
+		icon: "https://skillicons.dev/icons?i=js&theme=light",
+		proficiency: 3,
+		tags: ["インターン", "趣味"],
+	},
+	{
+		name: "Java",
+		icon: "https://skillicons.dev/icons?i=java&theme=light",
+		proficiency: 4,
+		tags: ["授業", "趣味"],
+	},
+	{
+		name: "Python",
+		icon: "https://skillicons.dev/icons?i=python&theme=light",
+		proficiency: 4,
+		tags: ["授業", "研究", "趣味", "ハッカソン"],
+	},
+	{
+		name: "Rust",
+		icon: "https://skillicons.dev/icons?i=rust&theme=light",
+		proficiency: 3,
+		tags: ["趣味"],
+	},
+	{
+		name: "TypeScript",
+		icon: "https://skillicons.dev/icons?i=ts&theme=light",
+		proficiency: 4,
+		tags: ["趣味", "ハッカソン"],
+	},
+];
+
+export const frameworks: TechnicalSkill[] = [
+	{
+		name: "Astro",
+		icon: "https://skillicons.dev/icons?i=astro&theme=light",
+		proficiency: 3,
+		tags: ["趣味"],
+	},
+	{
+		name: "Bootstrap",
+		icon: "https://skillicons.dev/icons?i=bootstrap&theme=light",
+		proficiency: 4,
+		tags: ["趣味"],
+	},
+	{
+		name: "FastAPI",
+		icon: "https://skillicons.dev/icons?i=fastapi&theme=light",
+		proficiency: 4,
+		tags: ["趣味", "ハッカソン"],
+	},
+	{
+		name: "Flask",
+		icon: "https://skillicons.dev/icons?i=flask&theme=light",
+		proficiency: 4,
+		tags: ["趣味", "ハッカソン"],
+	},
+	{
+		name: "Hono",
+		icon: "/image/skills/hono.jpg",
+		proficiency: 4,
+		tags: ["趣味"],
+	},
+	{
+		name: "jQuery",
+		icon: "https://skillicons.dev/icons?i=jquery&theme=light",
+		proficiency: 2,
+		tags: ["インターン"],
+	},
+	{
+		name: "Next.js",
+		icon: "https://skillicons.dev/icons?i=nextjs&theme=light",
+		proficiency: 3,
+		tags: ["趣味"],
+	},
+	{
+		name: "Rails",
+		icon: "https://skillicons.dev/icons?i=rails&theme=light",
+		proficiency: 4,
+		tags: ["インターン"],
+	},
+	{
+		name: "React",
+		icon: "https://skillicons.dev/icons?i=react&theme=light",
+		proficiency: 4,
+		tags: ["趣味", "ハッカソン"],
+	},
+	{
+		name: "Remix",
+		icon: "https://skillicons.dev/icons?i=remix&theme=light",
+		proficiency: 4,
+		tags: ["趣味"],
+	},
+	{
+		name: "scikit-learn",
+		icon: "https://skillicons.dev/icons?i=sklearn&theme=light",
+		proficiency: 3,
+		tags: ["授業"],
+	},
+	{
+		name: "Tailwind CSS",
+		icon: "https://skillicons.dev/icons?i=tailwind&theme=light",
+		proficiency: 4,
+		tags: ["ハッカソン"],
+	},
+	{
+		name: "Vitest",
+		icon: "https://skillicons.dev/icons?i=vitest&theme=light",
+		proficiency: 4,
+		tags: ["趣味"],
+	},
+];
+
+export const databases: TechnicalSkill[] = [
+	{
+		name: "Firebase",
+		icon: "https://skillicons.dev/icons?i=firebase&theme=light",
+		proficiency: 3,
+		tags: ["ハッカソン"],
+	},
+	{
+		name: "PostgreSQL",
+		icon: "https://skillicons.dev/icons?i=postgres&theme=light",
+		proficiency: 3,
+		tags: ["ハッカソン"],
+	},
+	{
+		name: "Redis",
+		icon: "https://skillicons.dev/icons?i=redis&theme=light",
+		proficiency: 3,
+		tags: ["ハッカソン"],
+	},
+	{
+		name: "SQLite",
+		icon: "https://skillicons.dev/icons?i=sqlite&theme=light",
+		proficiency: 4,
+		tags: ["授業", "趣味"],
+	},
+	{
+		name: "Supabase",
+		icon: "https://skillicons.dev/icons?i=supabase&theme=light",
+		proficiency: 3,
+		tags: ["趣味", "ハッカソン"],
+	},
+];
+
+export const infrastructures: TechnicalSkill[] = [
+	{
+		name: "AWS",
+		icon: "https://skillicons.dev/icons?i=aws&theme=light",
+		proficiency: 4,
+		tags: ["趣味", "ハッカソン"],
+	},
+	{
+		name: "GCP",
+		icon: "https://skillicons.dev/icons?i=gcp&theme=light",
+		proficiency: 3,
+		tags: ["ハッカソン"],
+	},
+	{
+		name: "Kubernetes",
+		icon: "https://skillicons.dev/icons?i=kubernetes&theme=light",
+		proficiency: 2,
+		tags: ["趣味"],
+	},
+];
+
+export const tools: TechnicalSkill[] = [
+	{
+		name: "Android Studio",
+		icon: "https://skillicons.dev/icons?i=androidstudio&theme=light",
+		proficiency: 2,
+		tags: ["趣味"],
+	},
+	{
+		name: "Cloudflare",
+		icon: "https://skillicons.dev/icons?i=cloudflare&theme=light",
+		proficiency: 4,
+		tags: ["趣味"],
+	},
+	{
+		name: "Docker",
+		icon: "https://skillicons.dev/icons?i=docker&theme=light",
+		proficiency: 4,
+		tags: ["研究", "趣味"],
+	},
+	{
+		name: "Drizzle ORM",
+		icon: "/image/skills/drizzle.png",
+		proficiency: 4,
+		tags: ["趣味"],
+	},
+	{
+		name: "Git",
+		icon: "https://skillicons.dev/icons?i=git&theme=light",
+		proficiency: 4,
+		tags: ["授業", "研究", "趣味"],
+	},
+	{
+		name: "GitHub",
+		icon: "https://skillicons.dev/icons?i=github&theme=light",
+		proficiency: 4,
+		tags: ["研究", "趣味"],
+	},
+	{
+		name: "GitHub Actions",
+		icon: "https://skillicons.dev/icons?i=githubactions&theme=light",
+		proficiency: 4,
+		tags: ["インターン", "趣味", "ハッカソン"],
+	},
+
+	{
+		name: "Prisma",
+		icon: "https://skillicons.dev/icons?i=prisma&theme=light",
+		proficiency: 3,
+		tags: ["趣味"],
+	},
+	{
+		name: "Prometheus",
+		icon: "https://skillicons.dev/icons?i=prometheus&theme=light",
+		proficiency: 2,
+		tags: ["趣味"],
+	},
+	{
+		name: "Postman",
+		icon: "https://skillicons.dev/icons?i=postman&theme=light",
+		proficiency: 3,
+		tags: ["インターン", "趣味"],
+	},
+	{
+		name: "Vercel",
+		icon: "https://skillicons.dev/icons?i=vercel&theme=light",
+		proficiency: 3,
+		tags: ["趣味", "ハッカソン"],
+	},
+	{
+		name: "Vim",
+		icon: "https://skillicons.dev/icons?i=vim&theme=light",
+		proficiency: 3,
+		tags: ["研究", "趣味"],
+	},
+	{
+		name: "VS Code",
+		icon: "https://skillicons.dev/icons?i=vscode&theme=light",
+		proficiency: 4,
+		tags: ["全般"],
+	},
+];
+
+export const proficiencyDescriptions = [
+	"自分で勉強したり、授業で習ったことがある程度で、プログラミングでの実装経験はない",
+	"詳しい人にサポートしてもらいながら、プログラミングでの実装経験がある",
+	"他の人のサポートなく、自身だけで実装した経験がある",
+	"授業や研究以外で、自身で実装し、オリジナルの成果物を作成した経験がある",
+	"自身でライブラリやフレームワークを作成することが可能",
+];
