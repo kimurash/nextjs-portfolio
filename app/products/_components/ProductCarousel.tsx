59bfe7c08b779420d39032114e5b3b49772a69f3
shunsei <k.shunsei1227@gmail.com>
2025-09-21 18:18:08 +0900

    add product carousel component

---
+++ app/products/_components/ProductCarousel.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import clsx from "clsx";
+import useEmblaCarousel from "embla-carousel-react";
+import Image from "next/image";
+import { usePrevNextButtons } from "../_hooks/usePrevNextButtons";
+import type { ProductImage } from "../types";
+import ProductCarouselNextButton from "./ProductCarouselNextButton";
+import ProductCarouselPrevButton from "./ProductCarouselPrevButton";
+
+interface Props {
+	productId: string;
+	images: ProductImage[];
+}
+
+const ProductCarousel = ({ productId, images }: Props) => {
+	const [emblaRef, emblaApi] = useEmblaCarousel({
+		loop: true,
+		align: "center", // FIXME: スライドが2枚以下の時に中央寄せにならない
+	});
+
+	const {
+		prevBtnDisabled,
+		nextBtnDisabled,
+		onPrevButtonClick,
+		onNextButtonClick,
+	} = usePrevNextButtons(emblaApi);
+
+	return (
+		<div className="flex flex-col">
+			<div className="overflow-hidden" ref={emblaRef}>
+				<div
+					className={
+						// biome-ignore format: preference
+						clsx(
+              "flex",
+              images.length < 2 && "justify-center",
+            )
+					}
+				>
+					{images.map((image, index) => (
+						<div
+							key={
+								// biome-ignore lint/suspicious/noArrayIndexKey: negligence
+								`${productId}-slide-${index}`
+							}
+							className="flex-[0_0_80%] min-w-0 mx-[10px]"
+						>
+							<Image
+								src={image.src}
+								alt={image.src}
+								width={image.width}
+								height={image.height}
+								className="h-auto w-full rounded-lg object-cover border"
+							/>
+						</div>
+					))}
+				</div>
+			</div>
+
+			<div className="mt-4">
+				<div className="flex items-center justify-center gap-4">
+					<ProductCarouselPrevButton
+						onClick={onPrevButtonClick}
+						disabled={prevBtnDisabled}
+					/>
+					<ProductCarouselNextButton
+						onClick={onNextButtonClick}
+						disabled={nextBtnDisabled}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ProductCarousel;
